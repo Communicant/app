f70cb4efebc3879af9568472772d517940596352
Anthony Ellis <ajellis1989@gmail.com>
2015-11-07 16:15:49 -0500

    Adds method to costs update which creates a new payment if that cost has been paid.

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -48,6 +48,10 @@ class CostsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @cost.errors, status: :unprocessable_entity }
       end
+      if @cost.paid = true
+          Payment.create!(title: @cost.title, amount: @cost.amount, parent_id: @cost.parent_id, created_at: Time.now, paid_at: Time.now, cost_id: @cost.id)
+        else
+      end
     end
   end
 
