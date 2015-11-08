4aed2a051a7a83a6daadbfcfc20e60a53487e5db
rosa <r215130@gmail.com>
2015-11-08 16:59:06 -0500

    working on users

--- db/migrate/20151108161237_create_users.rb
+++ db/migrate/20151108161237_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.type :type
+      t.string :type
       t.string :username
       t.string :password_digest
       t.string :email
