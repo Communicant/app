3ecf4aed82911081a371764673c52f271b5e7586
mamallama <reneeq25@gmail.com>
2015-11-07 20:43:28 -0500

    Events resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # root 'layouts' =>'layout#index'
   resources :payments
   resources :costs
+  resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
