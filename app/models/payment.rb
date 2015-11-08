2830429ecdb26aa47b52141c1324c2e8f9520990
Anthony Ellis <ajellis1989@gmail.com>
2015-11-07 19:56:21 -0500

    Adds methods for finding total cost amount and payment amount, and displas the information in html.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,11 @@
 class Payment < ActiveRecord::Base
+
+  def self.total_amount
+  all_payments = self.all
+  total_amount = 0
+  all_payments.each do |a|
+    total_amount += a.amount
+    end
+  total_amount
+  end
 end
