a971ea61c681b439864f33c62d0faaae4e10bf16
rosa <r215130@gmail.com>
2015-11-10 16:16:20 -0500

    playing with angular

--- app/assets/javascripts/messaging.js
+++ app/assets/javascripts/messaging.js
@@ -1,7 +1,7 @@
 ;(function(){
   var app = angular.module('Communicant', []);
    app.controller('ListOfMessagesController', ['$scope', '$http', function($scope, $http){
-     $http.get("/messages")
+     $http.get("/messages.json")
      .then(function(response){
       $scope.messages = response.data
        console.log("inside ListOfMessagesController")
