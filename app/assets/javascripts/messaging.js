799897f43d0f54fb5ad96c21581d70aacdaaa2af
rosa <r215130@gmail.com>
2015-11-10 16:47:34 -0500

    working more in angular, this time post

--- app/assets/javascripts/messaging.js
+++ app/assets/javascripts/messaging.js
@@ -1,13 +1,26 @@
 ;(function(){
   var app = angular.module('Communicant', []);
+  console.log("inside angular")
    app.controller('ListOfMessagesController', ['$scope', '$http', function($scope, $http){
      $http.get("/messages.json")
      .then(function(response){
       $scope.messages = response.data
        console.log("inside ListOfMessagesController")
      })
-  }]);
-  console.log("inside angular")
+   }]);//END of ListOfMessagesController
+  app.controller('NewMessageController', ['$scope', '$http', function($scope, $http){
+    console.log("inside NewMessageController")
+    $scope.submit = function(){
+    $http.post("/messages.json", $scope.newMessage); //I know it is not going to work for now
+    $scope.newMessage = { };
+    };
+    $http.get("/messages.json")
+     .then(function(response){
+       $scope.event = response.data;
+     })
+  }]);//END of NewMessageController
+
+
 
 
 
