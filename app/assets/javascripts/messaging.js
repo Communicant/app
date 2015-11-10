5336d1ceaff77e840434456105cab16dc4be3fa5
rosa <r215130@gmail.com>
2015-11-10 09:04:39 -0500

    playing with angular and the beatiful json file

--- app/assets/javascripts/messaging.js
+++ app/assets/javascripts/messaging.js
@@ -1,8 +1,12 @@
 ;(function(){
   var app = angular.module('Communicant', []);
-  // app.controller('ListOfMessages', ['$scope', '$http', function($scope, $http){
-  //   $http.get("")
-  //}]);
+   app.controller('ListOfMessagesController', ['$scope', '$http', function($scope, $http){
+     $http.get("fakemessages.json")
+     .then(function(response){
+      $scope.messages = response.data
+       console.log("inside ListOfMessagesController")
+     })
+  }]);
   console.log("inside angular")
 
 
