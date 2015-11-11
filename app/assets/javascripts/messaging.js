f53e5e8b4e5d0bb6a11e6018035cf4931e2c59df
rosa <r215130@gmail.com>
2015-11-11 17:04:36 -0500

    last commit for now

--- app/assets/javascripts/messaging.js
+++ app/assets/javascripts/messaging.js
@@ -2,14 +2,16 @@
   var app = angular.module('Communicant', ['ngRoute']);
   console.log("inside angular")
   app.controller('ListOfMessagesController', ['$scope', '$http', '$routeParams', '$timeout', '$interval', function($scope, $http, $routeParams, $timeout, $interval){
-     var Id = $routeParams.id - 1; //Id to make a difference with id
-     $http.get("/messages.json")
-     .then(function(response){
-      $scope.messages = response.data
-       console.log("inside ListOfMessagesController")
-       console.log(response.data.length)
+     //var Id = $routeParams.id - 1; //Id to make a difference with id
 
-     })
+     function updateMessages(){
+      return $http.get("/messages.json")
+        .then(function(response){
+          $scope.messages = response.data
+          console.log("inside ListOfMessagesController")
+          console.log(response.data.length)
+      })
+     }
 
      $interval(function(){
         $http.get("/messages.json")
@@ -26,21 +28,20 @@
     $scope.newMessage = { };
     $scope.submit = function(){
       console.log("after submit")
-      // $scope.messages.push({body:}) if I don't have json I can play with this.
       $http.post("/messages.json", $scope.newMessage)
-      .then(function(response){
-        $scope.newMessage = { };
-        console.log(response)
-        // TODO: What's in response? do i need that?
-       });
+        .then(function(response){
+          $scope.newMessage = { };
+          console.log('look what I got!', response)
+          // TODO: What's in response? do i need that?
+          // FIXME: API should return me the message I just created _as JSON_
+          // $scope.messages.push(response.data)
 
-       $timeout(function(){
-          $http.get("/messages.json").then(function(response){
-           $scope.messages = response.data;
-           console.log("inside the timeout")
-          //console.log("inside the get after the post, just trying to update the list of messages")
+          return $http.get("/messages.json").then(function(response){
+            $scope.messages = response.data;
+            console.log("inside the timeout")
+            //console.log("inside the get after the post, just trying to update the list of messages")
           })
-        }, 5000)
+        })
     };
 
   }]);//END of NewMessageController
