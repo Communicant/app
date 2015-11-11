799462afc7c99d9042f06c07b52a8f9e78749b15
Jasmine Baik <boshnivolo@gmail.com>
2015-11-11 02:09:39 -0500

    Code out exception

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,12 @@ class MessagesController < ApplicationController
 
   # GET /messages
   def index
-    messages = Message.all
-    messages = messages.map { |m| m.as_json.merge(name: User.find(m.user_id).first_name)}
-     render json: messages
+    @messages = Message.all
+    respond_to do |format|
+      format.html
+      format.json { @messages = @messages.map { |m| m.as_json.merge(name: User.find(m.user_id).first_name)}
+       render json: @messages }
+     end
   end
 
   # GET /messages/1
