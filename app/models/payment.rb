f57300fbd5c99922a1dc44fe099efc89f07714e8
Anthony Ellis <ajellis1989@gmail.com>
2015-11-11 14:29:40 -0500

    adds payments_total and amount_still_owed to expenses json

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,30 +3,9 @@ class Payment < ActiveRecord::Base
   belongs_to :user
 
   def self.total_amount
-  all_payments = self.all
-  total_amount = 0
-  all_payments.each do |a|
-    total_amount += a.amount
-    end
-  total_amount
+    self.sum(:amount).to_f
   end
 
 
-  def self.expense_payments_total(expense_id)
-    total_amount = 0
-    expense_payments = Payment.where(expense_id: expense_id)
-    expense_payments.each do |a|
-      total_amount += a.amount
-    end
-    total_amount
-  end
 
-  def self.expense_difference(expense_id)
-    total_amount = 0
-    expense_payments = Payment.where(expense_id: expense_id)
-    expense_payments.each do |a|
-      total_amount += a.amount
-    end
-    expense_difference = expense.amount - total_amount
-  end
 end
