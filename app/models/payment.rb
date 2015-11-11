60532b609e0b7571131c54abc2e862b91fb4275e
Anthony Ellis <ajellis1989@gmail.com>
2015-11-11 12:41:46 -0500

    Adds methods to find all payments for a single expense, and the total of all those payments.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,11 +1,29 @@
 class Payment < ActiveRecord::Base
+  belongs_to :expenses
+  belongs_to :user
 
-  # def self.total_amount
-  # all_payments = self.all
-  # total_amount = 0
-  # all_payments.each do |a|
-  #   total_amount += a.amount
-  #   end
-  # total_amount
-  # end
+  def self.total_amount
+  all_payments = self.all
+  total_amount = 0
+  all_payments.each do |a|
+    total_amount += a.amount
+    end
+  total_amount
+  end
+
+  def self.expense_payments
+    expense = Expense.find(params[:id])
+    expense_payments = Payment.where(expense_id: expense.id)
+    expense_payments
+  end
+
+  def self.expense_payments_total
+    expense = Expense.find(params[:id])
+    total_amount = 0
+    expense_payments = Payment.where(expense_id: expense.id)
+    expense_payments.each do |a|
+      total_amount += a.amount
+    end
+    total_amount
+  end
 end
