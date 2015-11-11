2be2faaa16d7d6a62e2583a107af18bbc52e3810
Anthony Ellis <ajellis1989@gmail.com>
2015-11-11 17:16:56 -0500

    Adds tests messages.

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -5,15 +5,4 @@ class MessagesControllerTest < ActionController::TestCase
     @message = messages(:one)
   end
 
-  test "new message can be saved" do
-    a = Message.new(user_id: 1, body: "hello!", date: Date.today, time: Time.now)
-    assert a.save
-  end
-
-  test "message must contain body" do
-    a = Message.new()
-    b = Message.new(body: nil)
-    assert a.save
-    refute b.save
-  end
 end
