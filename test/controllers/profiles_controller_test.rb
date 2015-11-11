84fd37d775338f5bae7f7c9b7e7950e7fcc217bf
Anthony Ellis <ajellis1989@gmail.com>
2015-11-11 16:40:11 -0500

    Adding tests.

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -5,45 +5,4 @@ class ProfilesControllerTest < ActionController::TestCase
     @profile = profiles(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:profiles)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create profile" do
-    assert_difference('Profile.count') do
-      post :create, profile: { case_number: @profile.case_number, child_name: @profile.child_name, email: @profile.email, mediator_name: @profile.mediator_name, name: @profile.name, phone_number: @profile.phone_number }
-    end
-
-    assert_redirected_to profile_path(assigns(:profile))
-  end
-
-  test "should show profile" do
-    get :show, id: @profile
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @profile
-    assert_response :success
-  end
-
-  test "should update profile" do
-    patch :update, id: @profile, profile: { case_number: @profile.case_number, child_name: @profile.child_name, email: @profile.email, mediator_name: @profile.mediator_name, name: @profile.name, phone_number: @profile.phone_number }
-    assert_redirected_to profile_path(assigns(:profile))
-  end
-
-  test "should destroy profile" do
-    assert_difference('Profile.count', -1) do
-      delete :destroy, id: @profile
-    end
-
-    assert_redirected_to profiles_path
-  end
 end
