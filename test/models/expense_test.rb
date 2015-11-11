ff154440ba4e22e1eaf0e3f5d5cf4bce51ee227c
Anthony Ellis <ajellis1989@gmail.com>
2015-11-11 17:51:48 -0500

    Adds tests for Expense methods.

--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @expense = expenses(:one)
+  end
+
+  test "can get total payments" do
+    assert_equal 20, @expense.payments_total
+  end
+
+  test "can get amount still owed" do
+    assert_equal 480, @expense.amount_still_owed
+  end
+
+  test "can get total amount" do
+    assert_equal 1000, Expense.total_amount
+  end
+
+  test "new Expense can be saved" do
+    a = Expense.new(type: "School", due_at: Date.today, amount: 450.00)
+    assert a.save
+  end
+
+  test "Expense must contain type" do
+    a = Expense.new(type: "School", due_at: Date.today, amount: 450.00)
+    b = Expense.new(due_at: Date.today, amount: 450.00)
+    assert a.save
+    assert_raises ActiveRecord::StatementInvalid do
+      b.save
+    end
+  end
+
+
 end
