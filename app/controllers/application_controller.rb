11e67f8049fcf29ac06b81b0bfd09e9b644f6de8
mamallama <reneeq25@gmail.com>
2015-11-13 00:11:04 -0500

    Login issues fixed for now, routes, child added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
 
+  before_filter :log_parent_in
+
+  def require_mediator
+    if !User.find(session[:user_id]).is_a?(Mediator)
+      redirect_to root_path, notice: "This area is for mediators only"
+    end
+  end
+  
+  def log_parent_in
+    session[:user_id] = User.first.id
+  end
+
   skip_before_action :verify_authenticity_token, if: :json_request?
 
 
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,4 +1,5 @@
 class Parent < User
+  has_secure_password
   has_many :messages
   has_one :case
 end
