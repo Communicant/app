ac88f4dac72fd7a5016a530cbe89441e83348420
Jasmine Baik <boshnivolo@gmail.com>
2015-11-12 19:12:58 -0500

    Houston we have angular

--- app/assets/javascripts/messaging.js
+++ app/assets/javascripts/messaging.js
@@ -1,50 +1,102 @@
 ;(function(){
-  var app = angular.module('Communicant', []);
+  var app = angular.module('Communicant', ['ngRoute']);
+  console.log("inside angular")
+  app.controller('ListOfMessagesController', ['$scope', '$http', '$routeParams', '$timeout', '$interval', function($scope, $http, $routeParams, $timeout, $interval){
+     //var Id = $routeParams.id - 1; //Id to make a difference with id
 
-  app.controller('ListOfMessagesController', ['$scope', '$http', function($scope, $http){
-    $http.get("/messages.json")
-    .then(function(response){
-      $scope.messages = response.data;
-    })
+     function updateMessages(){
+      return $http.get("/messages.json")
+        .then(function(response){
+          $scope.messages = response.data
+          console.log("inside ListOfMessagesController")
+          console.log(response.data.length)
+      })
+     }
 
-  }]); // END ListOfMessagesController
+     $interval(function(){
+        $http.get("/messages.json")
+        .then(function(response){
+        $scope.messages = response.data;
+        console.log("probando timeout")
+        //console.log("inside the get after the post, just trying to update the list of messages")
+        })
+      }, 5000);
 
-  app.controller('SendMessageController', ['$scope', '$http', function($scope, $http){
-    // console.log("Hello?");
+   }]);//END of ListOfMessagesController
+  app.controller('NewMessageController', ['$scope', '$http', '$timeout', '$interval',  function($scope, $http, $timeout, $interval){
+    console.log("inside NewMessageController")
     $scope.newMessage = { };
     $scope.submit = function(){
-      $http.post("/messages.json", $scope.newMessage);
+      console.log("after submit")
+      $http.post("/messages.json", $scope.newMessage)
+        .then(function(response){
+          $scope.newMessage = { };
+          console.log('look what I got!', response)
+          // TODO: What's in response? do i need that?
+          // FIXME: API should return me the message I just created _as JSON_
+          // $scope.messages.push(response.data)
 
-      $scope.newMessage = { };
+          return $http.get("/messages.json").then(function(response){
+            $scope.messages = response.data;
+            console.log("inside the timeout")
+            //console.log("inside the get after the post, just trying to update the list of messages")
+          })
+        })
     };
 
-    $http.get("/messages.json")
-    .then(function(response){
-      $scope.messages = response.data;
-    })
-  }]); // END SendMessageController
-
-//   app.controller('GiveNewMessageController', ['$scope', '$http', function dataCtrl($scope, $http, $timeout){
-//
-//     $scope.data = [];
-//
-//     (function tick() {
-//         $http.get("/messages.json").success(function (data) {
-//           console.log("Where are you $timeout???")
-//             $scope.data = data;
-//             $timeout(tick, 2000);
-//         });
-//     })();
-// }); // END timeout function
+  }]);//END of NewMessageController
 
 
-// $timeout([fn], [2000], [invokeApply], [Pass]);
+
+
+
 
   app.controller('ListExpensesController', ['$scope', '$http', function($scope, $http){
+    //Server call to get expenses
     $http.get("/expenses.json")
     .then(function(response){
       $scope.expenses = response.data;
     })
+
+    //New payment model
+    $scope.newPayment = {
+      payment : {
+        amount : 0,
+        expense_id : 0
+      }
+    };
+
+    //New expense model
+    $scope.newExpense = { };
+
+    $scope.submitPayment = function(expense_id){
+      $scope.newPayment.payment.expense_id = expense_id;
+      $http.post("/payments.json", $scope.newPayment).then(function(){
+        $http.get("/expenses.json")
+        .then(function(response){
+          $scope.expenses = response.data;
+        })
+        $scope.newPayment = { };
+      },function(err){
+        console.log(err);
+        //Server call failed
+      });
+    };
+
+    $scope.submitExpense = function(){
+      console.log('Submitting expense!!!');
+      $http.post("/expenses.json", $scope.newExpense).then(function(){
+        $http.get("/expenses.json")
+        .then(function(response){
+          $scope.expenses = response.data;
+        })
+      }, function(err){
+        console.log(err);
+        //Server call failed
+      });
+      $scope.newExpense = { };
+    };
+
   }]); // END ListExpensesController
 
   // app.controller('ListPaymentsController', ['$scope', '$http', function($scope, $http){
@@ -55,35 +107,21 @@
   //   })
   // }]); // END ListPaymentsController
 
-  app.controller('NewExpenseController', ['$scope', '$http', function($scope, $http){
-    // console.log("Hello?");
-    $scope.newExpense = { };
-    $scope.submit = function(){
-      $http.post("/expenses.json", $scope.newExpense);
+  // app.controller('NewExpenseController', ['$scope', '$http', function($scope, $http){
+  //   // console.log("Hello?");
+  //   $scope.newExpense = { };
+  //   $scope.submitExpense = function(){
+  //     $http.post("/expenses.json", $scope.newExpense);
+  //     $scope.newExpense = { };
+  //   };
+  //
+  //
+  //   $http.get("/expenses.json")
+  //   .then(function(response){
+  //     $scope.expenses = response.data;
+  //   })
+  // }]); // END NewExpenseController
 
-      $scope.newExpense = { };
-    };
-
-    $http.get("/expenses.json")
-    .then(function(response){
-      $scope.expenses = response.data;
-    })
-  }]); // END NewExpenseController
-
-  app.controller('NewPaymentController', ['$scope', '$http', function($scope, $http){
-
-    $scope.newPayment = { };
-    $scope.submit = function(){
-      $http.post("/payments.json", $scope.newPayment);
-
-      $scope.newPayment = { };
-    };
-
-    $http.get("/payments.json")
-    .then(function(response){
-      $scope.expenses = response.data;
-    })
-  }]); // END NewPaymentController
 
 })(); // END IIFE
 
