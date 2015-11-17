f0dae36645cdc06bfa54079e146bb95646f16560
Anthony Ellis <ajellis1989@gmail.com>
2015-11-17 11:43:24 -0500

    Fixed some of cases.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,31 +15,7 @@ class SessionsController < ApplicationController
         redirect_to root_path
       end
   end
-
-  # def create
-  #   case params[:sessions][:type]
-  #   when "Mediator"
-  #     mediator = Mediator.find_by_email(set_params[:email])
-  #       if mediator && mediator.authenticate(set_params[:password])
-  #         session[:user_id] = mediator.id
-  #
-  #       redirect_to mediator_path #TODO: MAKE VIEW FOR MEDIATORS!
-  #       else
-  #         redirect_to root, alert: "Login failed. Wrong email/password combination."
-  #       end
-  #   end
-  #
-  #   when "Parent"
-  #     parent = Parent.find_by_email(set_params[:email])
-  #     if parent && parent.authenticate(set_params[:password])
-  #       session[:user_id] = parent.id
-  #
-  #     redirect_to parent_path #TODO: MAKE VIEW FOR PARENT
-  #     else
-  #       redirect_to root, alert: "Login failed. Wrong email/password combination."
-  #     end
-  #   end
-  # end
+  
 
   def destroy
     session[:user_id] = nil
