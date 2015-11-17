6549d0237b67fe3f37d5d4c3f7c5607dd8a16de2
Jasmine Baik <boshnivolo@gmail.com>
2015-11-17 16:37:41 -0500

    Still don't understand schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116030712) do
+ActiveRecord::Schema.define(version: 20151116005050) do
 
   create_table "approvals", force: :cascade do |t|
     t.boolean  "parent_approval"
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(version: 20151116030712) do
     t.string   "child_name"
     t.integer  "case_number"
     t.string   "mediator_name"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "user_cases", force: :cascade do |t|
