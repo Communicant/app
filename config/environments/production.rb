98df523c9c3638f243238aa4f33a1442bd20bdd1
rosa <r215130@gmail.com>
2016-06-22 13:32:54 -0400

    removing line 18 in production for static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
 
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
   config.assets.compile = true
 
 
